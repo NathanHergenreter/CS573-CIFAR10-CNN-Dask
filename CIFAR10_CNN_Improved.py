c239d0eff3016c24747299e1b180ccbd14d5c376
Nathan Hergenreter <herg2019@iastate.edu>
2022-04-30 10:14:53 -0500

    Better VGG model

---
+++ CIFAR10_CNN_Improved.py
@@ -0,0 +1,48 @@
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import BatchNormalization
+from keras.layers import Conv2D
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Rescaling
+from tensorflow.data import AUTOTUNE
+from tensorflow.keras.optimizers import SGD
+from CIFAR10_CNN import cifar10_cnn
+
+class cifar10_cnn_improved(cifar10_cnn):
+    
+    
+    def define_model(self, lr, momentum):
+        model = Sequential()
+
+        model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(self.imgDimensions[0], self.imgDimensions[0], self.imgDimensions[1])))
+        model.add(BatchNormalization())
+        model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Dropout(0.2))
+        model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Dropout(0.3))
+        model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Dropout(0.4))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        model.add(Dense(10, activation='softmax'))
+
+        opt = SGD(learning_rate=lr, momentum=momentum)
+        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+        
+        return model
+        
